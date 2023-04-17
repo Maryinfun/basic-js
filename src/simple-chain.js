1ad4559c5df2b09d4fdf1c64c81691db26048642
Maryinfun <motmaryion@gmail.com>
2023-04-17 23:35:23 +0300

    fix: 18 tasks completed

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,41 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  smartChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.smartChain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.smartChain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      !Number.isInteger(position) ||
+      position < 1 ||
+      position > this.smartChain.length
+    ) {
+      this.smartChain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.smartChain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.smartChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const finish = this.smartChain.join("~~");
+    this.smartChain = [];
+    return finish;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
