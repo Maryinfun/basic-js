e7d80799765c1a3ae322b77dc9769b0f6f9c9239
Maryinfun <motmaryion@gmail.com>
2023-04-17 17:02:26 +0300

    check matrix-sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,11 +16,12 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
+function getMatrixElementsSum(matrix) {
   let sum = 0;
-  for (let i = 0; i < matrix[0].length; i++) {
-    for (let j = 0; j < matrix.length; j++) {
-      matrix[j][i] !== 0 ? (sum = sum + matrix[j][i]) : null;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[j][i] !== 0) sum += matrix[j][i];
+      else break;
     }
   }
   return sum;
