20a61a6d68737a3d9c2a26c400ed0b77f6ac8988
Maryinfun <motmaryion@gmail.com>
2023-04-17 18:56:20 +0300

    fix: 12 tasks done

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -18,8 +18,8 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 function getMatrixElementsSum(matrix) {
   let sum = 0;
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
       if (matrix[j][i] !== 0) sum += matrix[j][i];
       else break;
     }
